4e522d42f47ea57748ec53952d818c71fb66259c
Kim Daecheol <kdc9619@naver.com>
2025-02-24 21:41:58 +0900

    Feat: WebRTC 녹화 백엔드 구현
    - 녹화 핸들러 클래스 및 관련 클래스 정의
    - 설정 클래스에 Bean. 핸들러 등록

--- src/main/java/com/ssginc/showpinglive/config/WebSocketConfig.java
+++ src/main/java/com/ssginc/showpinglive/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.ssginc.showpinglive.config;
 
+import com.ssginc.showpinglive.handler.RecordHandler;
 import com.ssginc.showpinglive.handler.WebSocketHandler;
+import com.ssginc.showpinglive.util.UserRegistry;
 import org.kurento.client.KurentoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +14,25 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
     @Bean
     public WebSocketHandler webSocketHandler() {
         return new WebSocketHandler();
     }
 
     @Bean
+    public RecordHandler recordHandler() {
+        return new RecordHandler();
+    }
+
+    @Bean
     public KurentoClient kurentoClient() {
         return KurentoClient.create();
+    }
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
     }
 
     @Bean
@@ -32,5 +45,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler(), "/call");
+        registry.addHandler(recordHandler(), "/record");
     }
+
 }
