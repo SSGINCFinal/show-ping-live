63c11b33abfc386c9b8af1f198f213f31d0a9620
Kim Daecheol <kdc9619@naver.com>
2025-02-26 12:34:11 +0900

    Feat: VOD 파일 fetch 구현 #13
    - ResourceLoader 활용 VOD fetch 구현
    - 비동기 처리 로직인 WebFlux 적용

---
+++ src/main/java/com/ssginc/showpinglive/controller/StreamController.java
@@ -0,0 +1,73 @@
+package com.ssginc.showpinglive.controller;
+
+import com.ssginc.showpinglive.service.StreamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("stream")
+@RequiredArgsConstructor
+public class StreamController {
+
+    private final StreamService streamService;
+
+    @GetMapping(value = "/vod/fetch/{title}", produces = "video/mp4")
+    public Mono<ResponseEntity<Resource>> fetchVod(@PathVariable String title,
+                                                   @RequestHeader(value = "Range", required = false) String range) {
+
+        return streamService.getVideo(title)
+                .map(resource -> {
+                    long contentLength;
+                    try {
+                        contentLength = resource.contentLength();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
+                    if (range == null || range.isEmpty()) {
+                        return ResponseEntity.ok()
+                                .contentType(MediaTypeFactory.getMediaType(resource)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                                .body(resource);
+                    }
+
+                    String[] ranges = range.replace("bytes=", "").split("-");
+                    long start = Long.parseLong(ranges[0]);
+                    long end = ranges.length > 1 && !ranges[1].isEmpty() ? Long.parseLong(ranges[1]) : contentLength - 1;
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add("Content-Range", "bytes " + start + "-" + end + "/" + contentLength);
+                    headers.setContentLength(end - start + 1);
+                    headers.set(HttpHeaders.ACCEPT_RANGES, "bytes");
+
+                    InputStreamResource partialResource;
+                    try {
+                        partialResource = new InputStreamResource(resource.getInputStream()) {
+                            @Override
+                            public long contentLength() {
+                                return end - start + 1;
+                            }
+                        };
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                            .headers(headers)
+                            .contentType(MediaTypeFactory.getMediaType(resource)
+                                    .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                            .body(partialResource);
+                });
+    }
+}
--- src/main/java/com/ssginc/showpinglive/service/implement/StreamServiceImpl.java
+++ src/main/java/com/ssginc/showpinglive/service/implement/StreamServiceImpl.java
@@ -4,17 +4,35 @@ import com.ssginc.showpinglive.dto.response.VodResponseDto;
 import com.ssginc.showpinglive.repository.StreamRepository;
 import com.ssginc.showpinglive.service.StreamService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class StreamServiceImpl implements StreamService {
 
+    @Value("${download.path}")
+    private String VIDEO_PATH;
+
     private final StreamRepository streamRepository;
+
+    @Qualifier("webApplicationContext")
+    private final ResourceLoader resourceLoader;
 
     @Override
     public VodResponseDto getVodByNo(Long streamNo) {
         return streamRepository.findVodByNo(streamNo);
+    }
+
+    @Override
+    public Mono<Resource> getVideo(String title) {
+        String filePath = VIDEO_PATH + title + ".mp4";
+        return Mono.fromSupplier(() ->
+                resourceLoader.getResource(String.format(filePath)));
     }
 
 }
