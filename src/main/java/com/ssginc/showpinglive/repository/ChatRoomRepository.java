715c2d4eb5866fa07ecee281b8c87f80092b200e
Codeal <145662064+juil1-kim@users.noreply.github.com>
2025-03-07 10:34:48 +0900

    Revert "Feat: 기본 채팅 구현"

--- src/main/java/com/ssginc/showpinglive/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssginc.showpinglive.repository;
-
-import com.ssginc.showpinglive.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
