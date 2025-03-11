02a8d68268e253345d3421ef808063ee978a5c64
Kim Daecheol <kdc9619@naver.com>
2025-03-11 12:44:12 +0900

    Feat: 자막정보 클라이언트 표현 #36
    - 자막 활성화 여부에 따라 자막 표시 구현
    - track 태그를 활용 영상 내부에 자막 표시

--- src/main/resources/static/js/watch/vod.js
+++ src/main/resources/static/js/watch/vod.js
@@ -1,3 +1,7 @@
+let subtitles = [];
+const videoElement = document.getElementById('vod');
+const track = videoElement.addTextTrack("subtitles", "Korean", "ko");
+
 function addWatch(memberNo, streamNo) {
     const watchTime = new Date();
     
@@ -8,9 +12,28 @@ function addWatch(memberNo, streamNo) {
     });
 }
 
-async function streamVideo(title) {
-    const videoElement = document.getElementById('vod');
+function fetchSubtitle(title) {
+    track.mode = "disabled";
+    axios.get(`/stream/subtitle/${title}.json`)
+        .then(response => response.data)
+        .then(data => {
+            data.segments.forEach(segment => {
+                const cue = new VTTCue(
+                    msToSeconds(segment.start),
+                    msToSeconds(segment.end),
+                    segment.text
+                );
+                track.addCue(cue);
+            })
+        });
+}
 
+// 밀리초를 초 단위로 변환하는 함수
+function msToSeconds(ms) {
+    return ms / 1000;
+}
+
+async function streamVideo(title) {
     if (Hls.isSupported()) {
         var hls = new Hls();
         hls.loadSource(`/stream/vod/${title}.m3u8`);
@@ -18,5 +41,17 @@ async function streamVideo(title) {
     }
     else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
         videoElement.src = `/stream/vod/${title}.m3u8`;
+    }
+}
+
+function onSubtitle() {
+    if (track.mode === "disabled") {
+        track.mode = "showing";
+    }
+}
+
+function offSubtitle() {
+    if (track.mode === "showing") {
+        track.mode = "disabled";
     }
 }
