5babe710722366f8f67816b031234648d1eb2c24
juil1-kim <wndlfwndlf10@naver.com>
2025-03-12 10:36:11 +0900

    Refactor: Kafka Consumer 서비스 분리

--- src/main/java/com/ssginc/showpinglive/service/KafkaConsumerService.java
+++ src/main/java/com/ssginc/showpinglive/service/KafkaConsumerService.java
@@ -12,15 +12,7 @@ import org.springframework.stereotype.Service;
  * 몽고DB에 저장하는 클래스
  * <p>
  */
-@Service
-@RequiredArgsConstructor
-public class KafkaConsumerService {
-
-    private final ChatRepository chatRepository;
-    private final SimpMessagingTemplate messagingTemplate; // WebSocket 메시지 전송용
-
-    private long kafka_time = 0;
-    private long kafka_count = 0;
+public interface KafkaConsumerService {
 
     /**
      * Kafka에서 ChatDto 메시지를 소비하고 처리하는 메소드
@@ -29,30 +21,7 @@ public class KafkaConsumerService {
      *
      * @param chatDto Kafka에서 수신한 채팅 메시지 객체
      */
-    @KafkaListener(topics = "chat-messages", groupId = "chat-consumer-group")
-    public void consumeMessage(ChatDto chatDto) {
-        long startTime = System.currentTimeMillis();
-        kafka_count++;
 
-        System.out.println("Received chat message: " + chatDto);
-
-
-        try {
-            // 특정 채팅방(/sub/chat/room/{chatRoomNo})으로 메시지를 실시간 전송
-            messagingTemplate.convertAndSend("/sub/chat/room/" + chatDto.getChatRoomNo(), chatDto);
-            chatRepository.save(chatDto);
-            System.out.println(kafka_count + "받은 chat 몽고디비에 저장됨.");
-
-            long endTime = System.currentTimeMillis();
-            System.out.println(">>>>>>> 메세지 처리시간 : " + (endTime - startTime) + "ms");
-            kafka_time = endTime - startTime;
-
-            System.out.println(kafka_count + ">> kafka_time ==========> " + kafka_time);
-            System.out.println("Chat saved to MongoDB " + chatDto.getChatMessage());
-        } catch (Exception e) {
-            System.err.println("Error processing Kafka message: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+    void consumeMessage(ChatDto chatDto);
 
 }
