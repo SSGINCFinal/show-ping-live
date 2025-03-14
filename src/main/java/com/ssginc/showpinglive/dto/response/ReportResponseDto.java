39d40fc94e14a358a431748075284a47e5f6a54d
juil1-kim <wndlfwndlf10@naver.com>
2025-03-14 18:50:23 +0900

    Refactor: 신고 관리 상세정보 리팩토링
    - 신고 접수시 처리 여부가 바뀌지 않는 에러 해결

--- src/main/java/com/ssginc/showpinglive/dto/response/ReportResponseDto.java
+++ src/main/java/com/ssginc/showpinglive/dto/response/ReportResponseDto.java
@@ -12,6 +12,7 @@ public class ReportResponseDto {
     private Long reportNo;
     private String reportCreatedAt;  // 신고 생성일
     private String reportReason;
+    private String reportContent;
     private String memberId;
     private String reportStatus;
 //    private String messageCreatedAt; // 신고 메시지 작성 날짜
