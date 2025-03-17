fb5046d60458845c5fa807504c8351ca03a62e47
Kim Daecheol <kdc9619@naver.com>
2025-03-17 09:09:48 +0900

    Feat: 성능테스트 시나리오 3번 구현 #71
    - 웹플럭스가 아닌 버전으로 리소스 요청 구현

--- src/main/java/com/ssginc/showpinglive/controller/HlsController.java
+++ src/main/java/com/ssginc/showpinglive/controller/HlsController.java
@@ -2,6 +2,7 @@ package com.ssginc.showpinglive.controller;
 
 import com.ssginc.showpinglive.service.HlsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -71,10 +72,10 @@ public class HlsController {
      * @param title 영상 제목
      * @return TS 파일이 있는 응답객체 (확장자: ts)
      */
-    @GetMapping(value = "/v2/{title}.m3u8")
-    public Mono<?> getHLSV2(@PathVariable String title) {
+    @GetMapping(value = "/v2/flux/{title}.m3u8")
+    public Mono<?> getHLSV2Flux(@PathVariable String title) {
         // 불러온 ts 파일을 응답으로 전송
-        return hlsService.getHLSV2(title)
+        return hlsService.getHLSV2Flux(title)
                 .map(resource -> ResponseEntity.ok()
                         .header(HttpHeaders.CONTENT_TYPE, "video/mp2t")
                         .body(resource))
@@ -87,15 +88,53 @@ public class HlsController {
      * @param segment 세그먼트 번호
      * @return TS 파일이 있는 응답객체 (확장자: ts)
      */
-    @GetMapping(value = "/v2/{title}{segment}.ts")
-    public Mono<?> getTsSegmentV2(@PathVariable String title,
+    @GetMapping(value = "/v2/flux/{title}{segment}.ts")
+    public Mono<?> getTsSegmentV2Flux(@PathVariable String title,
                                   @PathVariable String segment) {
         // 불러온 ts 파일을 응답으로 전송
-        return hlsService.getTsSegmentV2(title, segment)
+        return hlsService.getTsSegmentV2Flux(title, segment)
                 .map(resource -> ResponseEntity.ok()
                         .header(HttpHeaders.CONTENT_TYPE, "video/mp2t")
                         .body(resource))
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    /**
+     * 영상 제목으로 HLS 파일(m3u8)을 NCP Storage에서 받아오는 컨트롤러 메서드
+     * @param title 영상 제목
+     * @return TS 파일을 포함한 응답 객체 (확장자: ts)
+     */
+    @GetMapping(value = "/v2/{title}.m3u8")
+    public ResponseEntity<?> getHLSV2(@PathVariable String title) {
+        // 동기 방식으로 리소스를 가져옵니다.
+        Resource resource = hlsService.getHLSV2(title);
+        if (resource != null) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "video/mp2t")
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * 영상 제목과 segment 번호로 TS 파일을 NCP Storage에서 받아오는 컨트롤러 메서드
+     * @param title 영상 제목
+     * @param segment 세그먼트 번호
+     * @return TS 파일을 포함한 응답 객체 (확장자: ts)
+     */
+    @GetMapping(value = "/v2/{title}{segment}.ts")
+    public ResponseEntity<?> getTsSegmentV2(@PathVariable String title,
+                                            @PathVariable String segment) {
+        // 동기 방식으로 리소스를 가져옵니다.
+        Resource resource = hlsService.getTsSegmentV2(title, segment);
+        if (resource != null) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "video/mp2t")
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
