8505d62242415b56c3da1043ba52510384155c18
Kim Daecheol <kdc9619@naver.com>
2025-03-24 01:51:24 +0900

    Refactor: 엔드포인트 수정
    - Nginx 이슈로 인한 엔드포인트 url 수정

--- src/main/resources/static/js/watch/vod.js
+++ src/main/resources/static/js/watch/vod.js
@@ -201,10 +201,10 @@ function msToSeconds(ms) {
 async function streamVideo(title) {
     if (Hls.isSupported()) {
         var hls = new Hls();
-        hls.loadSource(`/hls/v2/flux/${title}.m3u8`);
+        hls.loadSource(`/vod/v2/flux/${title}.m3u8`);
         hls.attachMedia(videoElement);
     } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
-        videoElement.src = `/stream/vod/${title}.m3u8`;
+        videoElement.src = `/vod/v2/flux/${title}.m3u8`;
     }
 }
 
